c61d2e619e896654ae3f8e820cf724b62e4d8c63
joelrpuchalski <112983728+joelrpuchalski@users.noreply.github.com>
2024-09-08 19:34:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "ChatGPT",
+        descricao: "O ChatGPT é um modelo de linguagem de grande porte desenvolvido pela OpenAI. Ele é capaz de gerar textos, traduzir idiomas, escrever diferentes tipos de conteúdo criativo e responder a suas perguntas de forma informativa. É como ter um assistente virtual inteligente sempre à disposição!",
+        link: "https://chat.openai.com/",
+        tags: "chat textos openAI"
+    },
+    {
+        titulo: "Google Gemini",
+        descricao: "O Google Gemini é a mais recente e poderosa ferramenta de inteligência artificial do Google. Desenvolvido para ser multi-modal, o Gemini pode processar e gerar diferentes tipos de dados, como texto, código, áudio, imagem e vídeo. Isso o torna extremamente versátil, capaz de realizar uma ampla gama de tarefas, desde a geração de textos criativos até a resolução de problemas complexos.",
+        link: "https://gemini.google.com/",
+        tags: "google gemini multi-modal"
+    },
+    {
+        titulo: "Bing",
+        descricao: "O Bing, da Microsoft, evoluiu para muito mais do que um simples buscador. Com a integração de inteligência artificial, ele se tornou um poderoso assistente virtual, capaz de te ajudar em diversas tarefas.",
+        link: "https://www.bing.com/?/ai",
+        tags: "bing microsoft buscador"
+    }
+];
